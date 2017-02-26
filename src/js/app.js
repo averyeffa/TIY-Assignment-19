4612ced2fe3e6385b72ecb85a142ba21be3d703a
Avery Effa <averyeffa@Averys-MacBook-Pro.local>
2017-02-26 14:51:22 -0500

    init

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,17 @@
-console.log('wat')
+import {githubApiKey} from '../../secrets.js'
+// import Backbone from 'backbone';
+import $ from 'jquery';
+
+var appContainerEl = document.querySelector("#app-container")
+
+
+
+var personalLogin = `averyeffa`
+var fetchUserProfileData = $.getJSON(`https://api.github.com/users/${personalLogin}`)
+var fetchUserRepoData = $.getJSON(`https://api.github.com/users/${personalLogin}/repos`)
+
+$.when(fetchUserProfileData, fetchUserRepoData).then(function(userProfileData, userRepoData){
+
+console.log(userProfileData[0])
+
+})

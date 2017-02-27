499a3f471e5bc60438eaebbdb080f10484dcccf8
Avery Effa <averyeffa@Averys-MacBook-Pro.local>
2017-02-27 16:06:33 -0500

    final

--- src/js/app.js
+++ src/js/app.js
@@ -26,7 +26,7 @@ inputValEl.addEventListener('keydown', function(evt){
       window.location.hash = inputValEntered
       userLogin = inputValEntered
       console.log(userLogin)
-      // Victor helped me here through the rest of this function 
+      // Victor helped me here through the rest of this function
       fetchUserProfileData = $.getJSON(`https://api.github.com/users/${userLogin}`)
       fetchUserRepoData = $.getJSON(`https://api.github.com/users/${userLogin}/repos`)
       // console.log(fetchUserRepoData, fetchUserProfileData)
@@ -45,10 +45,10 @@ function userHtmlTemplate(profileArray, repoArray){
   <div class="user-profile">
     <img src="${profileArray.avatar_url}">
     <h1>${profileArray.name}</h1>
-    <h1>${profileArray.login}</h1>
-    <h2>${profileArray.blog}</h2>
-    <h2>${profileArray.location}</h2>
-    <h2>${profileArray.email}</h2>
+    <h2>${profileArray.login}</h2>
+    <h3>${profileArray.blog}</h3>
+    <h3>${profileArray.location}</h3>
+    <h3>${profileArray.email}</h3>
 
     <button class="unfollow">Unfollow</button>
     <p>Block or report user</p>
@@ -65,9 +65,9 @@ function userHtmlTemplate(profileArray, repoArray){
 
       <div class="repo-details">
       <h2>${repoObj.name}</h2>
-      <h2>${repoObj.description}</h2>
-      <h3>${repoObj.language} <i class="fa fa-star" aria-hidden="true"></i>
-      ${repoObj.stargazers_count} ${repoObj.updated_at}</h3>
+      <h3>${repoObj.description}</h3>
+      <h4>${repoObj.language} <i class="fa fa-star" aria-hidden="true"></i>
+      ${repoObj.stargazers_count} <span>${repoObj.updated_at}</span></h4>
       </div>
     `
   })
@@ -76,18 +76,21 @@ function userHtmlTemplate(profileArray, repoArray){
   <div class="user-repo">
     <div class="repo-nav column-container">
     <h3>Overview</h3>
-    <h3>Repositories ${profileArray.public_repos}</h3>
+    <h3>Repositories <span>${profileArray.public_repos}</span></h3>
     <h3>Stars</h3>
-    <h3>Followers ${profileArray.followers}</h3>
-    <h3>Following ${profileArray.following}</h3>
+    <h3>Followers <span>${profileArray.followers}</span></h3>
+    <h3>Following <span>${profileArray.following}</span></h3>
     </div>
+
+    <hr/>
 
     <div class="repo-search column-container">
     <input class="search" type="text" value="" placeholder="Search repositories...">
     </input>
-    <button>Type</button>
-    <button>Language</button>
+    <button class="type">Type</button>
+    <button class="language">Language</button>
     </div>
+    <hr/>
   ` + repoHtmlStr + `</div>`
   // console.log(bigHtmlStr)
 
